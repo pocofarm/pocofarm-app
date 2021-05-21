0c576ab071c8c8aae8cb0182c699305f6688df1e
hyupee <5656sanghup@gmail.com>
2021-05-22 01:18:32 +0900

    feat: add Home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+export const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
